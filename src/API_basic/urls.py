61ed1bfc5a56b3ddc8028f2cb36afe0af3c8e691
dsvivass <dsvivass@unal.edu.co>
2021-09-07 15:59:28 -0500

    Se utilizó Viewsets and Routers

--- src/API_basic/urls.py
+++ src/API_basic/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsAPIView, GenericAPIView
+from django.urls import path, include
+from rest_framework import routers
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailsAPIView, GenericAPIView, ArticleViewSet
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     # path('article/', article_list),
@@ -7,4 +13,7 @@ urlpatterns = [
     # path('article/', ArticleAPIView.as_view()),
     # path('detail/<int:id>/', ArticleDetailsAPIView.as_view()),
     path('generic/article/<int:id>', GenericAPIView.as_view()),
+
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls)),
 ]
--- src/API_basic/views.py
+++ src/API_basic/views.py
@@ -26,8 +26,57 @@ from rest_framework import mixins
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+# Libs used for viewsets and routers
+
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+
+# VIEWSETS AND ROUTERS
+
+class ArticleViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+
+        articles = Article.objects.all()
+        serializers = ArticleModelSerializer(articles, many=True)
+
+        return Response(serializers.data)
+
+    def create(self, request):
+
+        serializer = ArticleModelSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None): # Trae solo 1 objeto con esa pk
+
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleModelSerializer(article)
+
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleModelSerializer(article, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
 
 # GENERIC VIEWS AND MIXIN, even better
 
